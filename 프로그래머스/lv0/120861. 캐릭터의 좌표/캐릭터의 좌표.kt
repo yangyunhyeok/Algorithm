b3acc27538a3997c6a4230eabdeffa72843c62d6
yangyunhyeok <139089275+yangyunhyeok@users.noreply.github.com>
2023-08-23 15:34:12 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.kt
@@ -0,0 +1,34 @@
+class Solution {
+    fun solution(keyinput: Array<String>, board: IntArray): IntArray {
+        val answer: IntArray = intArrayOf(0, 0)
+        for (i in keyinput) {
+            when (i) {
+                "left" -> {
+                    if (answer[0] <= -(board[0] / 2)) {
+                        continue
+                    }
+                    answer[0] -= 1
+                }
+                "right" -> {
+                    if (answer[0] >= board[0] / 2) {
+                        continue
+                    }
+                    answer[0] += 1
+                }
+                "up" -> {
+                    if (answer[1] >= board[1] / 2) {
+                        continue
+                    }
+                    answer[1] += 1
+                }
+                "down" -> {
+                    if (answer[1] <= -(board[1] / 2)) {
+                        continue
+                    }
+                    answer[1] -= 1
+                }
+            }
+        }
+        return answer
+    }
+}
